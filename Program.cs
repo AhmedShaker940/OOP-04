2aee3a525f6a35d05815ac9e7ade00da85532085
AhmedShaker940 <a7medsar7an123@gmail.com>
2026-08-18 04:32:25 +0300

    Q1  Abstraction

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part 01 — Theoretical Questions
+
+            #region Q1  Abstraction
+            //a)  What is Abstraction in Object-Oriented Programming?
+            /*
+               Abstraction is the process of hiding the implementation details and showing only the important features for users
+             */
+
+            //b)  Why is abstraction considered one of the four pillars of OOP?
+
+            /*
+             because it makes program reduces complexity of program , make it easier and faster to maintain besides it works on the whole program (methods , classes , props)
+             */
+            #endregion
+
+
+
+            #endregion
         }
     }
 }
